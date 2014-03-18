4780f4b0ef13e8fbe397c9b4fc504334b9732f5e
Dieko Bakker <diekobakker@hotmail.com>
2014-03-18 19:05:03 +0100

    add postfix functions, rework pre- and postfixes as switch statements

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
+	text = text.trim();
     suggest([
       {content: "http://reddit.com/r/" + text.replace(" ",""), description: "Goto /r/" + text.replace(" ","")},
 	  {content: "http://www.reddit.com/subreddits/search?q=" + text, description: "Search subreddits for " + text},
@@ -14,21 +15,93 @@ chrome.omnibox.onInputChanged.addListener(
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
 	var full_url;
-	if (text.substr(0,3) == "-s ") {
-		text = text.substr(3);
-		text = "http://www.reddit.com/subreddits/search?q=" + text;
-		}
-	else if (text.substr(0,3) == "-r ") {
-		text = text.substr(3);
-		text = "http://www.reddit.com/search?q=" + text;
-		}
-	else if (text.substr(0,4) == "-sd ") {
-		text = text.substr(4);
-		text = "https://duckduckgo.com/?q=reddit+" + text;
-		}
+	var splittext = text.split(" ");
+	var urlSet = false;
 	
-	if (text.substr(0,7) != "http://" && text.substr(0,8) != "https://") {full_url = "http://reddit.com/r/" + text.replace(" ","");	}
-	else {full_url = text;}
+	//detect prefixes
+	if (urlSet === false)
+	{
+		switch (splittext[0])
+		{
+			case "-s":
+				text = text.substr(3);
+				full_url = "http://www.reddit.com/subreddits/search?q=" + text;
+				urlSet = true;
+			break;
+			
+			case "-r":
+				text = text.substr(3);
+				full_url = "http://www.reddit.com/search?q=" + text;
+				urlSet = true;
+			break;
+			
+			case "-sd":
+				text = text.substr(4);
+				full_url = "https://duckduckgo.com/?q=reddit+" + text;
+				urlSet = true;
+			break;
+			
+			default:
+			//pass
+		};
+	};
+	
+	//detect postfixes
+	if (urlSet === false) 
+	{
+		switch (splittext[splittext.length - 1])
+		{
+			case "img":
+				text = text.substr(0,text.length - 3);
+				full_url = "http://imgur.com/r/" + text.replace(" ","");
+				urlSet = true;
+			break;
+			
+			case "top":
+				text = text.substr(0,text.length - 3);
+				full_url = "http://reddit.com/r/" + text.replace(" ","") + "/top/";
+				urlSet = true;
+			break;
+			
+			case "new":
+				text = text.substr(0,text.length - 3);
+				full_url = "http://reddit.com/r/" + text.replace(" ","") + "/new/";
+				urlSet = true;
+			break;
+			
+			case "hot": 
+				text = text.substr(0,text.length - 3);
+				full_url = "http://reddit.com/r/" + text.replace(" ","") + "/hot/";
+				urlSet = true;
+			break;
+			
+			case "rising":
+				text = text.substr(0,text.length - 6);
+				full_url = "http://reddit.com/r/" + text.replace(" ","") + "/rising/";
+				urlSet = true;
+			break;
+			
+			case "controversial":
+				text = text.substr(0,text.length - 13);
+				full_url = "http://reddit.com/r/" + text.replace(" ","") + "/controversial/";
+				urlSet = true;
+			break;
+			
+			case "wiki":
+				text = text.substr(0,text.length - 4);
+				full_url = "http://reddit.com/r/" + text.replace(" ","") + "/wiki/";
+				urlSet = true;
+			break;
+			
+			default:
+			//pass
+		};
+	};
+	
+	//detect if a web address has been generated at this point and set if necessary
+	if (urlSet === false) {full_url = "http://reddit.com/r/" + text.replace(" ","");}
+	
+	//goto web page
 	chrome.tabs.getSelected(null, function(tab)
 	{
 		chrome.tabs.update(tab.id, {url: full_url});
